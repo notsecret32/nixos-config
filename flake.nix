6a76347ff64f078d2779556fd8736f11c8d1496f
notsecret32 <sashaskvorhevskiy@gmail.com>
2025-07-18 10:53:10 +0300

    feat: added home manager

--- flake.nix
+++ flake.nix
@@ -3,9 +3,14 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: 
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: 
     let
       systemArch = "x86_64-linux";
       lib = nixpkgs.lib;
@@ -14,6 +19,12 @@
         system = systemArch;
         modules = [
           ./configuration.nix
+	  home-manager.nixosModules.home-manager
+	  {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.notsecret32 = import ./home.nix;
+	  }
         ];
       };
     };
---
+++ home.nix
@@ -0,0 +1,8 @@
+{ config, pkgs, ...}:
+
+{
+  home.username = "notsecret32";
+  home.homeDirectory = "/home/notsecret32";
+
+  home.stateVersion = "25.05";
+}
