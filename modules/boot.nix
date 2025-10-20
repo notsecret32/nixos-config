b43d8521ff0236fb56861093b2e52d2471c6d4d0
notsecret32 <43242733+notsecret32@users.noreply.github.com>
2025-10-20 22:13:30 +0300

    feat: NumLock autostart added

--- modules/boot.nix
+++ modules/boot.nix
@@ -9,7 +9,6 @@
         enable = true;
         device = "/dev/nvme0n1";
         useOSProber = true;
-        # Настройки для скрытого GRUB
         extraConfig = ''
           set timeout=1
           set timeout_style=hidden
@@ -19,14 +18,13 @@
         '';
         gfxmodeEfi = "auto";
         gfxpayloadEfi = "keep";
-        splashImage = null; # Или путь к вашему изображению
+        splashImage = null;
       };
       efi.canTouchEfiVariables = true;
       timeout = 1;
     };
 
     kernelParams = [
-      # Подавление вывода
       "quiet"
       "splash"
       "loglevel=0"
@@ -40,37 +38,35 @@
       "systemd.log_level=err"
       "systemd.log_target=null"
 
-      # Для Plymouth
       "plymouth.ignore-serial-consoles"
       "plymouth.enable=1"
       "fbcon=nodefer"
 
-      # Дополнительное подавление
       "acpi_osi=Linux"
       "acpi_backlight=vendor"
       "earlyprintk=off"
     ];
 
     consoleLogLevel = 0;
-    initrd.verbose = false;
 
-    # Инициализация Plymouth в initrd
-    initrd.systemd.enable = true;
+    initrd = {
+      verbose = false;
+      systemd.enable = true;
+      preLVMCommands = ''
+        ${pkgs.kbd}/bin/setleds +num
+      '';
+    };
 
-    # Plymouth тема
     plymouth = {
       enable = true;
-      theme = "bgrt"; # Лучшая тема для Windows-подобного опыта
-      # themePackages = [ pkgs.plymouth-themes ];
+      theme = "bgrt";
     };
   };
 
-  # Дополнительные системные настройки
   systemd.extraConfig = ''
     DefaultTimeoutStopSec=10s
   '';
 
-  # Отключить ненужные TTY во время загрузки
   systemd.services."getty@tty1".enable = false;
   systemd.services."getty@tty2".enable = false;
   systemd.services."getty@tty3".enable = false;
