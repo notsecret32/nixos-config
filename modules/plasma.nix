9391d5bc1115a37e6d518f85886eeaec87a41f38
notsecret32 <43242733+notsecret32@users.noreply.github.com>
2025-10-24 20:03:12 +0300

    feat: added custom resolution for CS2

--- modules/plasma.nix
+++ modules/plasma.nix
@@ -1,6 +1,16 @@
+{ pkgs, ... }:
+
 {
   services = {
-    xserver.enable = true;
+    xserver = {
+      enable = true;
+      displayManager = {
+        sessionCommands = ''
+          ${pkgs.xorg.xrandr}/bin/xrandr --newmode "1280x960_240.00"  476.10  1280 1392 1536 1792  960 961 964 1107  -HSync +Vsync
+        '';
+      };
+    };
+
     displayManager.sddm.enable = true;
     desktopManager.plasma6.enable = true;
   };
