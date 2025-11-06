c21bbe837e926c54f44308a2115d6c7530c6566e
notsecret32 <43242733+notsecret32@users.noreply.github.com>
2025-11-06 18:18:10 +0300

    feat: added vintage story

--- home-manager/home-packages.nix
+++ home-manager/home-packages.nix
@@ -8,6 +8,15 @@
     godot_4
     telegram-desktop
 
+    # Games
+    (vintagestory.overrideAttrs (oldAttrs: rec {
+      version = "1.21.5";
+      src = fetchurl {
+        url = "https://cdn.vintagestory.at/gamefiles/stable/vs_client_linux-x64_${version}.tar.gz";
+        hash = "sha256-dG1D2Buqht+bRyxx2ie34Z+U1bdKgi5R3w29BG/a5jg=";
+      };
+    }))
+
     # Nix
     nil
     nixfmt
